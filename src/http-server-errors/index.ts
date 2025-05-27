25bca5614595e4576393d032d39fb0b4fc989f06
Florian Patzke <fpatzke@ab-data.de>
2025-05-27 15:07:58 +0200

    feat: add HTTP client and server error classes with type definitions

---
+++ src/http-server-errors/index.ts
@@ -0,0 +1,27 @@
+import {
+  HttpInternalServerError,
+  HttpNotImplementedError,
+  HttpBadGatewayError,
+  HttpServiceUnavailableError,
+  HttpGatewayTimeoutError,
+  HttpHttpVersionNotSupportedError,
+  HttpVariantAlsoNegotiatesError,
+  HttpInsufficientStorageError,
+  HttpLoopDetectedError,
+  HttpNotExtendedError,
+  HttpNetworkAuthenticationRequiredError,
+} from "./http-server-errors";
+
+export {
+  HttpInternalServerError,
+  HttpNotImplementedError,
+  HttpBadGatewayError,
+  HttpServiceUnavailableError,
+  HttpGatewayTimeoutError,
+  HttpHttpVersionNotSupportedError,
+  HttpVariantAlsoNegotiatesError,
+  HttpInsufficientStorageError,
+  HttpLoopDetectedError,
+  HttpNotExtendedError,
+  HttpNetworkAuthenticationRequiredError,
+};

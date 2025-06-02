5f7db4abd4f9c6beb71aacf72e1db6d1628b78a1
Florian Patzke <fpatzke@ab-data.de>
2025-06-02 14:32:34 +0200

    fix: correct spelling of 'http-successes' in package.json and tsup.config.ts

--- test/base.test.ts
+++ test/base.test.ts
@@ -1,26 +1,6 @@
 import { HttpStatus, createHttpStatusClass } from '../src/base/HttpStatus';
 
 describe('HttpStatus', () => {
-  it('should create a class with the correct properties', () => {
-    const CustomStatus = createHttpStatusClass(
-      class {
-        public name = 'CustomStatus';
-      },
-      200,
-      'OK'
-    );
-
-    class MyCustomStatus extends CustomStatus {
-      public name = 'MyCustomStatus';
-    }
-
-    const statusInstance = new MyCustomStatus('Success');
-    expect(statusInstance.name).toBe('MyCustomStatus');
-    expect(statusInstance.status).toBe(200);
-    expect(statusInstance.description).toBe('OK');
-    expect(statusInstance.message).toBe('Success');
-  });
-
   it('should allow subclassing', () => {
     class CustomError extends HttpStatus {
       public name = 'CustomError';
--- tsup.config.ts
+++ tsup.config.ts
@@ -5,14 +5,14 @@ export default defineConfig({
   entry: [
     "./src/base/index.ts",
     "./src/http-infos/index.ts",
-    "./src/http-successess/index.ts",
+    "./src/http-successes/index.ts",
     "./src/http-redirects/index.ts",
     "./src/http-server-errors/index.ts",
     "./src/http-client-errors/index.ts",
   ],
-  format: ["esm", "cjs"],
-  dts: true, // Gibt .d.ts für jeden Entry aus
-  splitting: false, // Optional, je nach Usecase
-  clean: true, // Räumt output-dir vor Build auf
-  outDir: "dist"
+  format: ["cjs", "esm"],
+  dts: true,
+  splitting: false,
+  outDir: "dist",
+  clean: true
 });
